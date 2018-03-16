37dc7c0f438968881d5a36125110bfcf84b9628f
JackQinCh <jackqin3956@gmail.com>
2018-03-15 21:43:25 -0700

    Create FX interface for Basic Calculator

--- src/main/java/fx/BasicCalcFX.java
+++ src/main/java/fx/BasicCalcFX.java
@@ -6,13 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class BasicCalcFX extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("basic-calc.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
